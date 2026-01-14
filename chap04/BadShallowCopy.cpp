7577c1c20b242b3488a9332ceb73c2affe5af07c
Ksyear <ksy0305107@naver.ccom>
2026-01-14 11:21:36 +0000

    chap04 4.1.2

---
+++ chap04/BadShallowCopy.cpp
@@ -0,0 +1,22 @@
+//#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+//int _tmain(int argc, _TCHAR* argv){
+int main(int argc, char* argv[]){
+	int *pA, *pB;
+	
+	pA = new int;
+	*pA = 10;
+	
+	pB = new int;
+	pB = pA;
+	
+	cout << *pA << endl;
+	cout << *pB << endl;
+	
+	delete pA;
+	delete pB;
+	
+	return 0;
+}
---
+++ chap04/CopyConstructor1.cpp
@@ -0,0 +1,37 @@
+//#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class CMyData{
+public:
+	CMyData(){
+		cout << "CMyData()" << endl;
+	}
+	
+	CMyData(const CMyData &rhs) /* : m_nData(rhs.m_nData) */ {
+		this->m_nData = rhs.m_nData;
+		cout << "CMyData(const CMyData &)" << endl;
+	}
+	
+	int GetData(void) const {
+		return m_nData;
+	}
+	
+	void SetData(int nParam){
+		m_nData = nParam;
+	}
+	
+private:
+	int m_nData = 0;
+};
+
+//int _tmain(int argc, _TCHAR argv[]){
+int main(int argc, char* argv[]){
+	CMyData a;
+	a.SetData(10);
+	
+	CMyData b(a);
+	cout << b.GetData() << endl;
+	
+	return 0;
+}
